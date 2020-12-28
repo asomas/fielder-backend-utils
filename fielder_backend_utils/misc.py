2c948f4f36f643de1511891c66db12250b92d233
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-12-28 23:57:27 +0500

    Added task description

--- fielder_backend_utils/misc.py
+++ fielder_backend_utils/misc.py
@@ -1,10 +1,11 @@
 import requests
 
-def create_clickup_task(task_title, list_id, token):
+def create_clickup_task(task_title, list_id, token, task_description="n/a"):
     response = requests.post(
         f'https://api.clickup.com/api/v2/list/{list_id}/task/',
         json={
             'name': task_title,
+            'description': task_description,
         },
         headers={
             'Authorization': token
