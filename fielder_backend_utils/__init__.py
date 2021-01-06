0b61a6aadd36324d82cb37e6cb48493416d440d4
Yohanes <yohanes@asomas.ai>
2021-01-06 07:44:33 +0700

    added job shift recurrence related methods

--- fielder_backend_utils/__init__.py
+++ fielder_backend_utils/__init__.py
@@ -1,4 +1,8 @@
+import math
 from typing import List, Iterator
+from datetime import datetime, timedelta
+
+WEEKDAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
 def hello_fielder():
     print("Hello Fielder")
@@ -23,6 +27,44 @@ def make_chunks(item_list: List, chunk_size: int) -> Iterator:
         start = end
         end = start + chunk_size
         end = min(end, len(item_list))
+
+def next_weekday(d: datetime, weekday: int):
+    """
+    Get next weekday for given d datetime
+    If d is weekday, return d (not d+7)
+
+    Args:
+        d (datetime): reference date
+        weekday (int): 0=Monday, 1=Tuesday, 2=Wednesday...
+        interval (int): week interval
+    Returns
+        next_weekday (datetime): next weekday from d with given week_interval
+    """
+    days_ahead = weekday - d.weekday()
+    if days_ahead < 0: 
+        # Target day already happened this week
+        days_ahead += 7
+    return d + timedelta(days_ahead)
+
+def matches_reccurence(scheduled_date: datetime, recurrence: dict, start_date: datetime) -> bool:
+    """
+    Return True if a scheduled_date matches a reccurence rule that starts from start_date
+    Currently only support weekly recurrence type
+
+    Args:
+        scheduled_date (datetime): scheduled date
+        recurrence (dict): recurrence rule
+        start_date (datetime): recurrence start date
+    Returns:
+        match (bool)
+    """    
+    assert recurrence['repeat_interval_type'].lower() == 'weekly', 'only weekly type supported'
+    if not recurrence[WEEKDAYS[scheduled_date.weekday()]]:
+        return False
+    else:
+        first_scheduled_date = next_weekday(start_date, scheduled_date.weekday())
+        week_delta = math.ceil((scheduled_date - first_scheduled_date).days / 7)
+        return week_delta % recurrence['interval_amount'] == 0
 
 if __name__ == '__main__':
     pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
-from fielder_backend_utils import make_chunks
+from datetime import datetime
+from fielder_backend_utils import (
+    WEEKDAYS,
+    make_chunks,
+    next_weekday,
+    matches_reccurence,
+)
 
 class TestUtils(TestCase):
     def test_make_chunks(self):
@@ -12,3 +18,59 @@ class TestUtils(TestCase):
             end = min((i+1)*c, n)
             self.assertEqual(chunk, list(range(start, end)))
             i = i+1
+
+    def test_next_weekday(self):
+        self.assertEqual(next_weekday(datetime(2021,1,1), WEEKDAYS.index('saturday')), datetime(2021,1,2))
+        self.assertEqual(next_weekday(datetime(2021,1,1), WEEKDAYS.index('monday')), datetime(2021,1,4))
+        self.assertEqual(next_weekday(datetime(2021,1,1), WEEKDAYS.index('friday')), datetime(2021,1,1))
+
+    def test_matches_reccurence(self):
+        # negative: reject other repeat_interval_type other than Weekly
+        start_date = datetime(2021,1,1)
+        scheduled_date = datetime(2021,1,6)
+        reccurence = {
+            'repeat_interval_type': 'Monthly',
+            'interval_amount': 1,
+            'monday': True, 'tuesday': False, 'wednesday': False, 'thursday': False, 'friday': False, 'saturday': False, 'sunday': False,
+        }
+        self.assertRaises(AssertionError, matches_reccurence, scheduled_date, reccurence, start_date)
+
+        # positive
+        start_date = datetime(2021,1,1)
+        scheduled_date = datetime(2021,1,4)
+        reccurence = {
+            'repeat_interval_type': 'Weekly',
+            'interval_amount': 1,
+            'monday': True, 'tuesday': False, 'wednesday': False, 'thursday': False, 'friday': False, 'saturday': False, 'sunday': False,
+        }
+        self.assertTrue(matches_reccurence(scheduled_date, reccurence, start_date))
+
+        # negative: different day
+        start_date = datetime(2021,1,1)
+        scheduled_date = datetime(2021,1,5)
+        reccurence = {
+            'repeat_interval_type': 'Weekly',
+            'interval_amount': 1,
+            'monday': True, 'tuesday': False, 'wednesday': False, 'thursday': False, 'friday': False, 'saturday': False, 'sunday': False,
+        }
+        self.assertFalse(matches_reccurence(scheduled_date, reccurence, start_date))        
+
+        # positive: biweekly
+        start_date = datetime(2021,1,1)
+        scheduled_date = datetime(2021,1,18)
+        reccurence = {
+            'repeat_interval_type': 'Weekly',
+            'interval_amount': 2,
+            'monday': True, 'tuesday': False, 'wednesday': False, 'thursday': False, 'friday': False, 'saturday': False, 'sunday': False,
+        }
+        self.assertTrue(matches_reccurence(scheduled_date, reccurence, start_date))
+
+        # negative: biweekly, wrong week
+        start_date = datetime(2021,1,1)
+        scheduled_date = datetime(2021,1,11)
+        reccurence = {
+            'repeat_interval_type': 'Weekly',
+            'interval_amount': 2,
+            'monday': True, 'tuesday': False, 'wednesday': False, 'thursday': False, 'friday': False, 'saturday': False, 'sunday': False,
+        }
+        self.assertFalse(matches_reccurence(scheduled_date, reccurence, start_date))
