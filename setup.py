8e5c888c02423367b4865801fc34de4539c6bf35
Feras Almanna <firas.almanna3@gmail.com>
2021-06-25 22:42:55 +0300

    fix firebase emulator tests problem

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 install_requires = [
     "djangorestframework>=3.11.0",
-    "firebase-admin>=4.4.0",
-    "google-auth>=1.23.0",
+    "firebase-admin==4.4.0",
+    "google-cloud-firestore==2.0.2",
+    "google-auth>=1.23.0,<2.0dev",
+    "pyparsing<3",
     "pyjwt>=2.0.0",
     "requests>=2.25.1",
 ]
