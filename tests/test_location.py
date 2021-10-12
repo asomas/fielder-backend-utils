76194284abcfc0c06a3084b479a98bb350a27217
Feras Almanna <firas.almanna3@gmail.com>
2021-10-12 23:40:43 +0800

    update tests

--- tests/test_location.py
+++ tests/test_location.py
@@ -49,6 +49,58 @@ google_palce_api_response = {
     "status": "OK",
 }
 
+geocode_api_response = {
+    "results": [
+        {
+            "address_components": [
+                {"long_name": "1600", "short_name": "1600", "types": ["street_number"]},
+                {
+                    "long_name": "Amphitheatre Pkwy",
+                    "short_name": "Amphitheatre Pkwy",
+                    "types": ["route"],
+                },
+                {
+                    "long_name": "Mountain View",
+                    "short_name": "Mountain View",
+                    "types": ["locality", "political"],
+                },
+                {
+                    "long_name": "Santa Clara County",
+                    "short_name": "Santa Clara County",
+                    "types": ["administrative_area_level_2", "political"],
+                },
+                {
+                    "long_name": "California",
+                    "short_name": "CA",
+                    "types": ["administrative_area_level_1", "political"],
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": ["country", "political"],
+                },
+                {"long_name": "94043", "short_name": "94043", "types": ["postal_code"]},
+            ],
+            "formatted_address": "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA",
+            "geometry": {
+                "location": {"lat": 37.4224764, "lng": -122.0842499},
+                "location_type": "ROOFTOP",
+                "viewport": {
+                    "northeast": {"lat": 37.4238253802915, "lng": -122.0829009197085},
+                    "southwest": {"lat": 37.4211274197085, "lng": -122.0855988802915},
+                },
+            },
+            "place_id": "ChIJ2eUgeAK6j4ARbn5u_wAGqWA",
+            "plus_code": {
+                "compound_code": "CWC8+W5 Mountain View, California, United States",
+                "global_code": "849VCWC8+W5",
+            },
+            "types": ["street_address"],
+        }
+    ],
+    "status": "OK",
+}
+
 
 def mocked_requests_get(*args, **kwargs):
     class MockResponse:
@@ -56,6 +108,7 @@ def mocked_requests_get(*args, **kwargs):
             self.json_data = json_data
             self.status_code = status_code
             self.url = args[0]
+            self.ok = status_code == 200
 
         def json(self):
             return self.json_data
@@ -66,6 +119,12 @@ def mocked_requests_get(*args, **kwargs):
             and args[0] == "https://maps.googleapis.com/maps/api/place/details/json"
         ):
             return MockResponse(google_palce_api_response, 200)
+        elif (
+            kwargs["params"]["formatted_address"]
+            == "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA"
+            and args[0] == "https://maps.googleapis.com/maps/api/geocode/json"
+        ):
+            return MockResponse(geocode_api_response, 200)
         return MockResponse(None, 404)
     return MockResponse(None, 403)
 
@@ -154,4 +213,15 @@ class TestLocation(TestCase):
         }
         self.assertDictEqual(
             google_place_details("GOOGLE_PLACE_ID", "API_SECRET"), expected_location
+        )
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_geocode_api(self, mock_get):
+        expected_coorsd = {"lat": 37.4224764, "lng": -122.0842499}
+
+        self.assertEqual(
+            geocode(
+                "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA", "API_SECRET"
+            ),
+            expected_coorsd,
         )
