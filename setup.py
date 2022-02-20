4599f6fc4502096959bc07c8fb44005913a13d2a
Feras Almanna <firas.almanna3@gmail.com>
2022-02-20 18:58:51 +0300

    upgrade firebase-admin and google-cloud-firestore

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from fielder_backend_utils import __version__
 
 install_requires = [
     "djangorestframework>=3.11.0",
-    "firebase-admin==4.4.0",
-    "google-cloud-firestore==2.2.0",
+    "firebase-admin==5.2.0",
+    "google-cloud-firestore==2.3.4",
     "google-cloud-tasks==2.0.0",
     "google-cloud-pubsub==2.2.0",
     "google-auth>=1.23.0,<2.0dev",
