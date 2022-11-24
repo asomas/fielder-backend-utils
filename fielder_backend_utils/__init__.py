d376f0674f24d567057125d5e490716de2a67592
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-11-24 13:07:07 +0500

    Upgraded packages to latest versions

--- fielder_backend_utils/__init__.py
+++ fielder_backend_utils/__init__.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Any, Dict, Iterator, List
 
-__version__ = "1.0.76"
+__version__ = "1.0.77"
 
 WEEKDAYS = [
     "monday",
--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@ from setuptools import find_packages, setup
 from fielder_backend_utils import __version__
 
 install_requires = [
-    "djangorestframework>=3.11.0",
-    "firebase-admin==5.2.0",
-    "google-cloud-firestore>=2.1.0,<2.2.0",
-    "google-cloud-tasks==2.0.0",
-    "google-cloud-pubsub==2.2.0",
-    "google-auth>=1.23.0,<2.0dev",
-    "pyparsing<3",
-    "pyjwt>=2.0.0",
-    "lxml==4.6.4",
-    "requests>=2.25.1",
-    "protobuf==3.19.0",
+    "black~=22.10.0",
+    "wheel~=0.38.0",
+    "djangorestframework~=3.12.4",
+    "Django~=4.1.2",
+    "django-cors-headers~=3.13.0",
+    "firebase-admin~=6.0.1",
+    "google-cloud-firestore~=2.7.2",
+    "google-cloud-secret-manager~=2.12.6",
+    "google-cloud-tasks~=2.10.4",
+    "google-cloud-pubsub~=2.13.10",
+    "google-auth~=2.13.0",
+    "gunicorn~=20.1.0",
+    "lxml~=4.9.0",
+    "pyjwt~=2.6.0",
+    "pyparsing~=3.0.9",
+    "python-i18n~=0.3.9",
+    "requests~=2.28.1",
+    "sentry-sdk~=1.9.10",
 ]
 
 setup(
